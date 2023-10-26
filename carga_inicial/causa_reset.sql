dbd88ce852697ca64f6c9823e163aaf5fbaa7309
Andres Galaz <andres.galaz@gmail.com>
2023-10-26 18:45:33 -0300

    Carga causas

--- carga_inicial/causa_reset.sql
+++ carga_inicial/causa_reset.sql
@@ -1,2 +1,5 @@
+truncate table public.legales_abogado cascade;
+ALTER SEQUENCE public.legales_abogado_id_seq RESTART WITH 1;
+
 truncate table public.legales_causa cascade;
 ALTER SEQUENCE public.legales_causa_id_seq RESTART WITH 1;
